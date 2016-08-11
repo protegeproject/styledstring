8026e8838925d5b38b850e4c59f69894edda352d
Matthew Horridge <matthew.horridge@stanford.edu>
2016-08-11 12:38:25 -0700

    Added a flag for a repaint request

---
+++ src/main/java/edu/stanford/bmir/styledstring/swing/RepaintRequest.java
@@ -0,0 +1,13 @@
+package edu.stanford.bmir.styledstring.swing;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 11 Aug 16
+ */
+public enum RepaintRequest {
+
+    REPAINT,
+
+    DO_NOT_REPAINT
+}
